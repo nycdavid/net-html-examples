f7f0915eb92cf0943488bc990819f0e9f7f2f443
David Ko <david.ko@pvtmethod.com>
2017-12-28 08:46:47 -0500

    Listing all tags

--- main.go
+++ main.go
@@ -24,5 +24,25 @@ var markup = `
 
 func main() {
 	r := strings.NewReader(markup)
-	html.NewTokenizer(r)
+	z := html.NewTokenizer(r)
+
+	/* 1. */
+	ListAllTagTypes(z)
+}
+
+// Example of printing all types of tags in an HTML string
+func ListAllTagTypes(z *html.Tokenizer) {
+	for {
+		tt := z.Next()
+
+		if tt == html.ErrorToken {
+			return
+		}
+
+		switch tt {
+		case html.StartTagToken:
+			tag, _ := z.TagName()
+			fmt.Println(string(tag))
+		}
+	}
 }
